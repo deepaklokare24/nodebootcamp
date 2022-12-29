60a5c7fc23f2429c67e41886951401b201ce3362
Deepak Lokare <deepaklokare.24@gmail.com>
2022-12-29 11:39:05 +0530

    resolving callback hell problem using promises

--- asynchronous/dog-ceo.js
+++ asynchronous/dog-ceo.js
@@ -1,18 +1,41 @@
 const fs = require('fs')
 const superagent = require('superagent')
 
-fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, breed) => {
-  console.log('dog breed is ', breed)
-
-  if (breed) {
-    superagent.get(`https://dog.ceo/api/breed/${breed}/images/random`).end((err, dogImage) => {
-      console.log(dogImage.body.message)
-
-      fs.writeFile(`${__dirname}/../txt/dogfile.txt`, dogImage.body.message, (err) => {
-        if (err) return console.log(err.message)
-
-        console.log('Dog breed image is successfully saved to file')
-      })
+const readfilePromise = (fileName) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+      if (err) reject('I could not find that file 😭')
+      resolve(data)
     })
-  }
-})
+  })
+}
+
+const writefilePromise = (fileName, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(fileName, data, (err) => {
+      if (err) reject(err.message)
+      resolve('Dog breed image is successfully saved to file')
+    })
+  })
+}
+
+let readFilePath = `${__dirname}/dog.txt`
+let writeFilePath = `${__dirname}/../txt/dogfile.txt`
+
+readfilePromise(readFilePath)
+  .then((breed) => {
+    if (breed) {
+      console.log(`Breed:: ${breed}`)
+      return superagent.get(`https://dog.ceo/api/breed/${breed}/images/random`)
+    }
+  })
+  .then((result) => {
+    let data = result.body.message
+    return writefilePromise(writeFilePath, data)
+  })
+  .then((result) => {
+    console.log('Success !!! 😃', result)
+  })
+  .catch((error) => {
+    console.log('Failure !!! 😢', error)
+  })
